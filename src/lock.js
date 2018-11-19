bec2a5d58c48599f27d56e20efe4c8a4fcb54924
Hugo Dias <mail@hugodias.me>
2018-11-19 17:50:57 +0000

    fix: fix lock for node 11 (#181)
    * fix: fix lock for node 11
    
    * fix: remove unused _isLocked method
    
    * chore: fix lint error

--- src/lock.js
+++ src/lock.js
@@ -1,14 +1,12 @@
 'use strict'
 
-const Lock = require('lock-me')
 const path = require('path')
 const debug = require('debug')
-const fs = require('fs')
+const { lock } = require('proper-lockfile')
 
 const log = debug('repo:lock')
 
 const lockFile = 'repo.lock'
-const lock = new Lock()
 
 /**
  * Lock the repo in the given dir.
@@ -20,36 +18,14 @@ const lock = new Lock()
 exports.lock = (dir, callback) => {
   const file = path.join(dir, lockFile)
   log('locking %s', file)
-  lock(file, callback)
-}
 
-/**
- * Check if the repo in the given directory is locked.
- *
- * @param {string} dir
- * @param {function(Error, bool)} callback
- * @returns {void}
- */
-exports.locked = (dir, callback) => {
-  const file = path.join(dir, lockFile)
-  log('checking lock: %s')
-
-  if (!fs.existsSync(file)) {
-    log('file does not exist: %s', file)
-  }
-
-  lock(file, (err, lck) => {
-    if (err) {
-      log('already locked: %s', err.message)
-      return callback(null, true)
-    }
-
-    log('no one has a lock')
-    lck.close((err) => {
-      if (err) {
-        return callback(err)
-      }
-      callback(null, false)
+  lock(dir, {lockfilePath: file})
+    .then(release => {
+      callback(null, {close: (cb) => {
+        release()
+          .then(() => cb())
+          .catch(err => cb(err))
+      }})
     })
-  })
+    .catch(err => callback(err))
 }
