15b2a6d312d57c1ae9bd5ad37d2d339006579d00
achingbrain <alex@achingbrain.net>
2020-05-07 18:44:50 +0100

    feat: use streaming API for blockstore access
    Uses the streaming APIs from interface-datastore to work with data
    being sent/fetched from the blockstore.
    
    The previous behaviour was to use batches for putting streams of blocks
    - this was bad because we'd end up buffering the whole stream before
    adding blocks to the blockstore.  Instead now we stream the blocks
    as we get them.

--- src/blockstore.js
+++ src/blockstore.js
@@ -23,25 +23,25 @@ function createBaseStore (store) {
     /**
      * Query the store.
      *
-     * @param {object} query
-     * @return {Iterable}
+     * @param {Object} query
+     * @param {Object} options
+     * @returns {AsyncIterator<Block>}
      */
-    async * query (query) {
-      for await (const block of store.query(query)) {
-        yield block
-      }
+    async * query (query, options) { // eslint-disable-line require-await
+      yield * store.query(query, options)
     },
     /**
      * Get a single block by CID.
      *
      * @param {CID} cid
+     * @param {Object} options
      * @returns {Promise<Block>}
      */
-    async get (cid) {
+    async get (cid, options) {
       const key = cidToKey(cid)
       let blockData
       try {
-        blockData = await store.get(key)
+        blockData = await store.get(key, options)
         return new Block(blockData, cid)
       } catch (err) {
         if (err.code === 'ERR_NOT_FOUND') {
@@ -52,7 +52,7 @@ function createBaseStore (store) {
           }
 
           const otherKey = cidToKey(otherCid)
-          const blockData = await store.get(otherKey)
+          const blockData = await store.get(otherKey, options)
           await store.put(key, blockData)
           return new Block(blockData, cid)
         }
@@ -61,79 +61,92 @@ function createBaseStore (store) {
       }
     },
     /**
+     * Like get, but for more.
+     *
+     * @param {AsyncIterator<CID>} cids
+     * @param {Object} options
+     * @returns {AsyncIterator<Block>}
+     */
+    async * getMany (cids, options) {
+      for await (const cid of cids) {
+        yield this.get(cid, options)
+      }
+    },
+    /**
      * Write a single block to the store.
      *
      * @param {Block} block
+     * @param {Object} options
      * @returns {Promise<void>}
      */
-    async put (block) {
+    async put (block, options) {
       if (!Block.isBlock(block)) {
         throw new Error('invalid block')
       }
 
       const k = cidToKey(block.cid)
-      const exists = await store.has(k)
+      const exists = await store.has(k, options)
       if (exists) return
-      return store.put(k, block.data)
+      return store.put(k, block.data, options)
     },
 
     /**
      * Like put, but for more.
      *
      * @param {AsyncIterable<Block>|Iterable<Block>} blocks
+     * @param {Object} options
      * @returns {Promise<void>}
      */
-    async putMany (blocks) {
-      const batch = store.batch()
+    async * putMany (blocks, options) { // eslint-disable-line require-await
+      yield * store.putMany((async function * () {
+        for await (const block of blocks) {
+          const key = cidToKey(block.cid)
 
-      for await (const block of blocks) {
-        const key = cidToKey(block.cid)
+          if (await store.has(key, options)) {
+            continue
+          }
 
-        if (await store.has(key)) {
-          continue
+          yield { key, value: block.data }
         }
-
-        batch.put(key, block.data)
-      }
-
-      return batch.commit()
+      }()), options)
     },
     /**
      * Does the store contain block with this cid?
      *
      * @param {CID} cid
+     * @param {Object} options
      * @returns {Promise<bool>}
      */
-    async has (cid) {
-      const exists = await store.has(cidToKey(cid))
+    async has (cid, options) {
+      const exists = await store.has(cidToKey(cid), options)
       if (exists) return exists
       const otherCid = cidToOtherVersion(cid)
       if (!otherCid) return false
-      return store.has(cidToKey(otherCid))
+      return store.has(cidToKey(otherCid), options)
     },
     /**
      * Delete a block from the store
      *
      * @param {CID} cid
+     * @param {Object} options
      * @returns {Promise<void>}
      */
-    async delete (cid) { // eslint-disable-line require-await
-      return store.delete(cidToKey(cid))
+    async delete (cid, options) { // eslint-disable-line require-await
+      return store.delete(cidToKey(cid), options)
     },
     /**
      * Delete a block from the store
      *
      * @param {AsyncIterable<CID>} cids
+     * @param {Object} options
      * @returns {Promise<void>}
      */
-    async deleteMany (cids) {
-      const batch = store.batch()
-
-      for await (const cid of cids) {
-        batch.delete(cidToKey(cid))
-      }
-
-      return batch.commit()
+    async * deleteMany (cids, options) { // eslint-disable-line require-await
+      yield * store.deleteMany((async function * () {
+        for await (const cid of cids) {
+          yield cidToKey(cid)
+        }
+      }()), options)
     },
     /**
      * Close the store
