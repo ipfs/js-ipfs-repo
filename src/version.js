efa72b7caba97c85d9e85db87d5ab10fdaa1ad8b
achingbrain <alex@achingbrain.net>
2021-01-27 16:40:11 +0000

    chore: upgrade to latest datastore-level
    BREAKING CHANGE: requires repo migration to v10

--- src/version.js
+++ src/version.js
@@ -5,6 +5,10 @@ const debug = require('debug')
 const log = debug('ipfs:repo:version')
 const uint8ArrayToString = require('uint8arrays/to-string')
 const uint8ArrayFromString = require('uint8arrays/from-string')
+const {
+  hasWithFallback,
+  getWithFallback
+} = require('ipfs-repo-migrations/src/utils')
 
 const versionKey = new Key('version')
 
@@ -16,7 +20,10 @@ module.exports = (store) => {
      * @returns {Promise<bool>}
      */
     async exists () { // eslint-disable-line require-await
-      return store.has(versionKey)
+      // level-js@5.x cannot read keys from level-js@4.x dbs so fall back to
+      // using IndexedDB API with string keys - only necessary until we do
+      // the migratiion to v10 or above
+      return hasWithFallback(versionKey, store.has.bind(store), store)
     },
     /**
      * Get the current version.
@@ -24,7 +31,10 @@ module.exports = (store) => {
      * @returns {Promise<Integer>}
      */
     async get () {
-      const buf = await store.get(versionKey)
+      // level-js@5.x cannot read keys from level-js@4.x dbs so fall back to
+      // using IndexedDB API with string keys - only necessary until we do
+      // the migratiion to v10 or above
+      const buf = await getWithFallback(versionKey, store.get.bind(store), store.has.bind(store), store)
       return parseInt(uint8ArrayToString(buf), 10)
     },
     /**
