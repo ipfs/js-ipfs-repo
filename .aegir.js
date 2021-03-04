2cde613484a53a927f1628c971e140889d375f41
achingbrain <alex@achingbrain.net>
2021-03-04 15:26:00 +0000

    chore: revert change and use migrations with added browser field

--- .aegir.js
+++ .aegir.js
@@ -11,10 +11,6 @@ const esbuild = {
         build.onResolve({ filter: /^stream$/ }, () => {
           return { path: require.resolve('readable-stream') }
         })
-        build.onResolve({ filter: /^cborg$/ }, () => {
-          // remove when https://github.com/evanw/esbuild/issues/187 is fixed
-          return { path: require.resolve('cborg') }
-        })
       }
     }
   ]
