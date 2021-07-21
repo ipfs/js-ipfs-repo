df23866fa5ed619d0cd8ab7212f20da386d76f34
achingbrain <alex@achingbrain.net>
2021-07-21 16:40:24 +0100

    fix: walk dag for all supported codecs
    Use the `.links()` function from the multiformats block class, no need
    to reinvent the wheel.

--- src/utils/walk-dag.js
+++ src/utils/walk-dag.js
@@ -1,12 +1,10 @@
 'use strict'
 
-const { CID } = require('multiformats/cid')
-const cborg = require('cborg')
-const dagPb = require('@ipld/dag-pb')
-const dagCbor = require('@ipld/dag-cbor')
 const log = require('debug')('ipfs:repo:utils:walk-dag')
+const Block = require('multiformats/block')
 
 /**
+ * @typedef {import('multiformats/cid').CID} CID
  * @typedef {import('interface-blockstore').Blockstore} Blockstore
  * @typedef {import('../types').loadCodec} loadCodec
  * @typedef {import('../types').AbortOptions} AbortOptions
@@ -21,65 +19,18 @@ const log = require('debug')('ipfs:repo:utils:walk-dag')
  */
 async function * walkDag (cid, blockstore, loadCodec, options) {
   try {
-    const block = await blockstore.get(cid, options)
+    const bytes = await blockstore.get(cid, options)
     const codec = await loadCodec(cid.code)
-    const node = codec.decode(block)
+    const block = Block.createUnsafe({ bytes, cid, codec })
 
-    if (cid.code === dagPb.code) {
-      for (const link of node.Links) {
-        yield link.Hash
-        yield * walkDag(link.Hash, blockstore, loadCodec, options)
-      }
-    } else if (cid.code === dagCbor.code) {
-      for (const [, childCid] of dagCborLinks(node)) {
-        yield childCid
-        yield * walkDag(childCid, blockstore, loadCodec, options)
-      }
+    for (const [, childCid] of block.links()) {
+      yield childCid
+      yield * walkDag(childCid, blockstore, loadCodec, options)
     }
   } catch (err) {
     log('Could not walk DAG for CID', cid.toString(), err)
 
     throw err
-  }
-}
-
-// eslint-disable-next-line jsdoc/require-returns-check
-/**
- * @param {any} obj
- * @param {string[]} path
- * @param {boolean} parseBuffer
- * @returns {Generator<[string, CID], void, undefined>}
- */
-function * dagCborLinks (obj, path = [], parseBuffer = true) {
-  if (parseBuffer && obj instanceof Uint8Array) {
-    obj = cborg.decode(obj)
-  }
-
-  for (const key of Object.keys(obj)) {
-    const _path = path.slice()
-    _path.push(key)
-    const val = obj[key]
-
-    if (val && typeof val === 'object') {
-      if (Array.isArray(val)) {
-        for (let i = 0; i < val.length; i++) {
-          const __path = _path.slice()
-          __path.push(i.toString())
-          const o = val[i]
-          if (o instanceof CID) { // eslint-disable-line max-depth
-            yield [__path.join('/'), o]
-          } else if (typeof o === 'object') {
-            yield * dagCborLinks(o, _path, false)
-          }
-        }
-      } else {
-        if (val instanceof CID) {
-          yield [_path.join('/'), val]
-        } else {
-          yield * dagCborLinks(val, _path, false)
-        }
-      }
-    }
   }
 }
 
